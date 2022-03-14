25b10286c64f32e9806d9bd18a16bd9ec0b1fc8f
jianfeng <gjf610@yeah.net>
2022-03-14 12:26:10 +0800

    feat: render label

--- docs/component/tree/index.md
+++ docs/component/tree/index.md
@@ -7,7 +7,41 @@
 :::demo
 ```vue
 <template>
-<z-tree :data="[{label: '中国菜系'}]"></z-tree>
+<z-tree :data="[{
+      label: '一级 1',
+      children: [{
+        label: '二级 1-1',
+        children: [{
+          label: '三级 1-1-1'
+        }]
+      }]
+    }, {
+      label: '一级 2',
+      children: [{
+        label: '二级 2-1',
+        children: [{
+          label: '三级 2-1-1'
+        }]
+      }, {
+        label: '二级 2-2',
+        children: [{
+          label: '三级 2-2-1'
+        }]
+      }]
+    }, {
+      label: '一级 3',
+      children: [{
+        label: '二级 3-1',
+        children: [{
+          label: '三级 3-1-1'
+        }]
+      }, {
+        label: '二级 3-2',
+        children: [{
+          label: '三级 3-2-1'
+        }]
+      }]
+    }]"></z-tree>
 </template>
 ```
 :::
--- zioui/tree/src/tree.tsx
+++ zioui/tree/src/tree.tsx
@@ -1,6 +1,6 @@
 import './tree.scss'
 
-import { defineComponent, render } from 'vue'
+import { defineComponent } from 'vue'
 import { treeProps, TreeProps } from './tree-types'
 
 export default defineComponent({
@@ -8,8 +8,24 @@ export default defineComponent({
   props: treeProps,
   setup(props: TreeProps, ctx) {
     console.log(props.data)
+    const renderNode = (item) => <div>{item.label}</div>
+    const renderTree = (treeData) => {
+      return treeData.map(item => {
+        if (item.children) {
+          return (
+            <>
+              {renderNode(item)}
+              {renderTree(item.children)}
+            </>
+          )
+        }
+        return renderNode(item)
+      })
+    }
     return () => (
-      <div class="z-tree">{props.data.map(item => <div>{item.label}</div>)}</div>
+      <div class="z-tree">
+        {renderTree(props.data)}
+      </div>
     )
   }
 })
