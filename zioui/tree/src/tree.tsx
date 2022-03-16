c68e673af1d6ace15f2b9264a1613737b7b3b46a
jianfeng <gjf610@yeah.net>
2022-03-16 19:24:24 +0800

    feat: added toggle

--- zioui/tree/src/tree.tsx
+++ zioui/tree/src/tree.tsx
@@ -3,11 +3,12 @@ import './tree'
 import { defineComponent } from 'vue'
 import { treeProps, TreeProps } from './tree-types'
 import IconOpen from '../../../src/assets/open'
+import IconClose from '../../../src/assets/close'
+
 export default defineComponent({
   name: 'tree',
   props: treeProps,
   setup(props: TreeProps, ctx) {
-
     const openedTree = (tree) => {
       return tree.reduce((acc, item) => (
         item.open
@@ -15,31 +16,31 @@ export default defineComponent({
           : acc.concat(item)
       ), [])
     }
-
-    const openedData = openedTree(props.data)
-
-    console.log(openedData)
+    let openedData = openedTree(props.data)
+    const toggle = (item) => {
+      if (!item.children) {
+        return
+      }
+      item.open = !item.open
+      openedData = openedTree(props.data)
+      console.log('toggle', openedData)
+    }
     // 缩进
     const Indent = () => <span style='display: inline-block;width: 16px'></span>
     const renderNode = (item) => (
       <div style={{ paddingLeft: 20 * (item.level - 1) + 'px' }}>
-        {item.children ? <IconOpen /> : <Indent />}
+        {
+          item.children
+            ? item.open
+              ? <IconOpen onClick={() => toggle(item)} />
+              : <IconClose onClick={() => toggle(item)} />
+            : <Indent />
+        }
         {item.label}
       </div>
     )
     const renderTree = (treeData) => {
       return treeData.map(item => renderNode(item))
-      // return treeData.map(item => {
-      //   if (item.children) {
-      //     return (
-      //       <>
-      //         {renderNode(item)}
-      //         {renderTree(item.children)}
-      //       </>
-      //     )
-      //   }
-      //   return renderNode(item)
-      // })
     }
     return () => (
       <div class="z-tree">
